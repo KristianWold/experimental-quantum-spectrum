7272cbdaccd74c0291da0bdabe800074c3815f92
KristianWold <kriswold@student.matnat.uio.no>
2022-05-19 22:47:38 +0200

    Implement Kraus form with unitary part

--- src.py
+++ src.py
@@ -47,27 +47,6 @@ def state_norm(A, B):
 
 
 #@profile
-def apply_map(state, choi):
-    d = state.shape[0]
-    #A = np.random.normal(0, 1, (d,d))
-    #reg = A@A.T# + 1e-8*A
-
-    #reshuffle
-    choi = choi.reshape(d,d,d,d).swapaxes(1,2).reshape(d**2, d**2)
-
-    #flatten
-    state = state.reshape(-1, 1)
-
-    state = (choi@state).reshape(d, d)
-    return state
-
-def apply_kraus(state, kraus_list):
-    state = sum([K@state@K.T.conj() for K in kraus_list])
-
-    return state
-
-
-#@profile
 def prepare_input(config, return_unitary = False):
     """1 = |0>, 2 = |1>, 3 = |+>, 4 = |->, 5 = |+i>, 6 = |-i>"""
     n = len(config)
@@ -117,7 +96,6 @@ def likelihood(state, data):
     pass
 
 
-
 #@profile
 def generate_ginibre(dim1, dim2, real = False):
     ginibre = np.random.normal(0, 1, (dim1, dim2))
@@ -133,70 +111,138 @@ def generate_state(dim1, dim2):
     return state
 
 
-#@profile
-def generate_choi(X):
-    d = int(np.sqrt(X.shape[0]))  # dim of Hilbert space
-    I = np.eye(d)
-    XX = X@X.conj().T
-    #partial trace
-    Y = partial_trace(XX)
-    Y = sqrtm(Y)
-    Y = np.linalg.inv(Y)
-    Ykron = np.kron(I, Y)
+def generate_unitary(ginibre):
+    Q, R = np.linalg.qr(ginibre)
+    R = np.diag(R)
+    sign = R/np.abs(R)
+    U = Q@np.diag(sign)
 
-    #choi
-    choi = Ykron@XX@Ykron
-
-    return choi
-
-def generate_kraus(X, d, rank):
-    #U, _, _ = torch.svd(X)
-    U, _ = np.linalg.qr(X)
-    #U = Q@torch.diag(torch.sgn(torch.diag(R)))
-    kraus_list = [U[i*d:(i+1)*d, :d] for i in range(rank)]
-
-    return kraus_list
+    return U
 
 
 class Adam():
-    def __init__(self, dims, lr=0.01, beta1=0.9, beta2=0.999, eps=1e-8):
+
+    def __init__(self, lr=0.01, beta1=0.9, beta2=0.999, eps=1e-8):
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
         self.eps = eps
 
+        self.m = []
+        self.v = []
         self.t = 0
-        self.m = np.zeros(dims, dtype="complex128")
-        self.v = np.zeros(dims, dtype="complex128")
 
 
-    def __call__(self, gradient):
+    def __call__(self, weight_gradient_list, lr):
+        if len(self.m) == 0:
+            for weight_gradient in weight_gradient_list:
+                self.m.append(np.zeros_like(weight_gradient))
+                self.v.append(np.zeros_like(weight_gradient))
+
         self.t += 1
+        weight_gradient_modified = []
 
-        self.m = self.beta1 * self.m + (1 - self.beta1) * gradient
-        self.v = self.beta2 * self.v + (1 - self.beta2) * np.abs(gradient)**2
+        for grad, m_, v_ in zip(weight_gradient_list, self.m, self.v):
+            m_[:] = self.beta1 * m_ + (1 - self.beta1) * grad
+            v_[:] = self.beta2 * v_ + (1 - self.beta2) * grad*grad.conj()
 
-        m_hat = self.m / (1 - self.beta1**self.t)
-        v_hat = self.v / (1 - self.beta2**self.t)
-        gradient_modified = m_hat / (np.sqrt(v_hat) + self.eps)
+            m_hat = m_ / (1 - self.beta1**self.t)
+            v_hat = v_ / (1 - self.beta2**self.t)
+            grad_modified = m_hat / (np.sqrt(v_hat) + self.eps)
+            weight_gradient_modified.append(lr*grad_modified)
 
-        return gradient_modified
+        return weight_gradient_modified
+
+
+class ChoiMap():
+
+    def __init__(self, d, rank):
+        self.d = d
+        self.rank = rank
+
+        self.ginibre = generate_ginibre(d**2, rank)
+        self.parameter_list = [self.ginibre]
+
+        self.choi = None
+        self.generate_map()
+
+    def apply_map(self, state):
+        d = self.d
+
+        #reshuffle
+        choi = self.choi.reshape(d,d,d,d).swapaxes(1,2).reshape(d**2, d**2)
+
+        #flatten
+        state = state.reshape(-1, 1)
+
+        state = (choi@state).reshape(d, d)
+        return state
+
+    def generate_map(self):
+        I = np.eye(self.d)
+        X = self.ginibre
+        XX = X@X.conj().T
+        #partial trace
+        Y = partial_trace(XX)
+        Y = sqrtm(Y)
+        Y = np.linalg.inv(Y)
+        Ykron = np.kron(I, Y)
+
+        #choi
+        self.choi = Ykron@XX@Ykron
+
+    def update_parameters(self, weight_gradient_list):
+        for parameter, weight_gradient in zip(self.parameter_list, weight_gradient_list):
+            parameter += weight_gradient
+
+        self.generate_map()
+
+
+class KrausMap():
+
+    def __init__(self, U, c, d, rank):
+        self.U = U
+        self.d = d
+        self.rank = rank
+
+        self.ginibre = generate_ginibre(rank*d, d)
+        k = -np.log(1/c - 1)
+        self.k = np.array([k], dtype = "float64")
+        self.parameter_list = [self.ginibre, self.k]
+
+        self.kraus_list = None
+        self.generate_map()
+
+    def apply_map(self, state):
+        c = 1/(1 + np.exp(-self.k[0]))
+        state = c*self.U@state@self.U.T.conj() + (1 - c)*sum([K@state@K.T.conj() for K in self.kraus_list])
+        return state
+
+    def generate_map(self):
+        d = self.d
+        U = generate_unitary(self.ginibre)
+        self.kraus_list = [U[i*d:(i+1)*d, :d] for i in range(self.rank)]
+
+    def update_parameters(self, weight_gradient_list):
+        for parameter, weight_gradient in zip(self.parameter_list, weight_gradient_list):
+            parameter += weight_gradient
+
+        self.generate_map()
 
 
 class ModelQuantumMap:
 
-    def __init__(self, n, rank, state_input_list, state_target_list, lr, h):
-        self.n = n
-        self.rank = rank
+    def __init__(self, model, state_input_list, state_target_list, lr, h):
+        self.model = model
         self.state_input_list = state_input_list
         self.state_target_list = state_target_list
         self.lr = lr
         self.h = h
 
-        self.d = 2**n
-        self.X_model = generate_ginibre(self.d**2, self.rank)
+        self.d = model.d
+        self.rank = model.rank
 
-        self.adam = Adam(dims = (self.d**2, self.rank))
+        self.adam = Adam()
         self.fid_list = []
 
 #    @profile
@@ -207,28 +253,39 @@ class ModelQuantumMap:
             self.state_input = self.state_input_list[index]
             self.state_target = self.state_target_list[index]
 
-            # Finite difference over all parameters
-            choi_model = generate_choi(self.X_model)
-            state_model = apply_map(self.state_input, choi_model)
-            self.fid_zero = state_norm(state_model, self.state_target)
-            fidd = state_fidelity(state_model, self.state_target)
+            state_model = self.model.apply_map(self.state_input)
+            self.fid_zero = state_fidelity(state_model, self.state_target)
 
-            grad_matrix = np.zeros_like(self.X_model)
+            grad_matrix = np.zeros_like(self.model.ginibre)
             for idx in range(self.d**2*self.rank):
-                i = idx//self.rank
-                j = idx%self.rank
+                mod_val = self.model.ginibre.shape[1]
+                i = idx//mod_val
+                j = idx%mod_val
                 grad_matrix[i, j] = self.calculate_gradient(i, j)
 
             grad_matrix /= self.h
 
+            grad_list = [grad_matrix]
+
+            if len(self.model.parameter_list) == 2:
+                self.model.parameter_list[1][0] += self.h
+                state_plus = self.model.apply_map(self.state_input)
+                fid_plus = state_fidelity(state_plus, self.state_target)
+                self.model.parameter_list[1][0] -= self.h
+
+                grad = (fid_plus - self.fid_zero)/self.h
+                grad = np.array([grad])
+
+                grad_list.append(grad)
+
             if use_adam:
-                grad_matrix = self.adam(grad_matrix)
+                grad_list = self.adam(grad_list, self.lr)
 
-
-            self.X_model += self.lr*grad_matrix
+            self.model.update_parameters(grad_list)
+            c = 1/(1 + np.exp(-self.model.k[0]))
 
             self.fid_list.append(self.fid_zero)
-            print(f"{step}: {self.fid_zero:.3f}, {fidd:.3f}")
+            print(f"{step}: fid: {self.fid_zero:.3f}, c: {c:.3f}")
 
 #    @profile
     def calculate_gradient(self, i, j):
@@ -236,23 +293,22 @@ class ModelQuantumMap:
         h = self.h
         state_input = self.state_input
         state_target = self.state_target
-        X_model = self.X_model
 
         #Finite difference, real value
-        X_model[i, j] += h
-        choi_plus = generate_choi(X_model)
-        state_plus = apply_map(state_input, choi_plus)
-        fid_plus = state_norm(state_plus, state_target)
-        X_model[i, j] -= h
+        self.model.ginibre[i, j] += h
+        self.model.generate_map()
+        state_plus = self.model.apply_map(state_input)
+        fid_plus = state_fidelity(state_plus, state_target)
+        self.model.ginibre[i, j] -= h
 
         grad = (fid_plus-self.fid_zero)
 
         #Finite difference, imaginary value
-        X_model[i, j] += 1j*h
-        choi_plus = generate_choi(X_model)
-        state_plus = apply_map(state_input, choi_plus)
-        fid_plus = state_norm(state_plus, state_target)
-        X_model[i, j] -= 1j*h
+        self.model.ginibre[i, j] += 1j*h
+        self.model.generate_map()
+        state_plus = self.model.apply_map(state_input)
+        fid_plus = state_fidelity(state_plus, state_target)
+        self.model.ginibre[i, j] -= 1j*h
 
         grad += 1j*(fid_plus-self.fid_zero)
 
