127594a654b06af73a43b1b0339590b30eb7db87
Kristian Wold <krisw@oslomet.no>
2023-02-17 13:56:12 +0100

    Add Hamiltonian noise mitigation benchmark

--- src_tf/lindblad_channels.py
+++ src_tf/lindblad_channels.py
@@ -173,3 +173,86 @@ class ExplicitLindbladMap(Channel):
     @property
     def choi(self):
         return reshuffle(self.super_operator)
+
+
+
+class TracelessLindbladMap(Channel):
+    def __init__(
+        self,
+        d=None,
+        rank=None,
+        weight=None,
+        spam=None,
+        trainable=True,
+        jump_operators_trainable=True,
+        generate=True,
+    ):
+
+        self.d = d
+        self.rank = rank
+        self.weight = weight
+        self.I = tf.cast(tf.eye(d), dtype=precision)
+
+        if spam is None:
+            spam = IdealSPAM(d)
+        self.spam = spam
+
+        _, A, B = generate_ginibre(d, d, trainable=trainable)
+        self.H_params = [tf.cast(A, dtype=tf.double), tf.cast(B, dtype=tf.double)]
+
+        self.A_list = []
+        self.B_list = []
+        for i in range(rank - 1):
+            _, A, B = generate_ginibre(d, d, trainable=jump_operators_trainable)
+            self.A_list.append(tf.cast(A, dtype=tf.double))
+            self.B_list.append(tf.cast(B, dtype=tf.double))
+
+        self.parameter_list = self.H_params
+
+        if jump_operators_trainable:
+            self.parameter_list += self.A_list + self.B_list
+        
+
+        self.super_operator = None
+        if generate:
+            self.generate_channel()
+            
+
+    def generate_channel(self):
+        H = self.generate_hamiltonian()
+        L_list = self.generate_jump_operators()
+
+        HH = -1j * (kron(H, self.I) - kron(self.I, tf.math.conj(H)))
+        LL = 0
+        for i, L in enumerate(L_list):
+            L2 = tf.matmul(L, L, adjoint_a=True)
+            LL += (
+                kron(L, tf.math.conj(L))
+                - 0.5 * ((kron(L2, self.I) + kron(self.I, tf.math.conj(L2))))
+            )
+
+        self.super_operator = tf.linalg.expm(HH + self.weight * LL)
+
+    def apply_channel(self, state):
+        state = tf.reshape(state, (-1, d**2, 1))
+        state = tf.matmul(self.super_operator, state)
+        state = tf.reshape(state, (-1, d, d))
+
+        return state
+
+    def generate_hamiltonian(self):
+        G = tf.complex(self.H_params[0], self.H_params[1])
+        H = G + tf.linalg.adjoint(G)
+        return H
+
+    def generate_jump_operators(self):
+        L_list = [tf.complex(A, B) for A, B in zip(self.A_list, self.B_list)]
+
+        L_list = [L-tf.linalg.trace(L) for L in L_list]
+
+        return L_list
+
+    @property
+    def choi(self):
+        return reshuffle(self.super_operator)
+
