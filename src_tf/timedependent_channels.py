d8d93c601772b3fcfde9e9baf6c780ac70eb3e16
Kristian Wold <krisw@oslomet.no>
2023-03-09 19:28:55 +0100

    Add midpoint Magnus Prop

--- src_tf/timedependent_channels.py
+++ src_tf/timedependent_channels.py
@@ -17,35 +17,96 @@ from set_precision import *
 
 
 class Liouvillian():
-    def __init__(self,)
+    def __init__(self):
         pass
 
     def __call__(self, t):
         pass
 
-class SpinSpin(Liouvillian):
-    def __init__(self, u):
+
+class Hamiltonian(Liouvillian):
+    def __init__(self,):
         self.d = 4
         self.rank = 1
-        self.u = tf.Variable(u, trainable=True)
-        #self.x = tf.Variable(0, trainable=True)
-        #self.y = tf.Variable(0, trainable=True)
-        #self.z = tf.Variable(0, trainable=True)
+        self.u1 = tf.Variable(tf.random.normal(0,1), trainable=True)
+        self.u2 = tf.Variable(tf.random.normal(0,1), trainable=True)
+        self.u3 = tf.Variable(tf.random.normal(0,1), trainable=True)
 
-        self.parameter_list = [self.u]
+        self.parameter_list = [self.u1, self.u2, self.u3]
 
     def __call__(self, t):
         X = tf.Variable([[0, 1], [1, 0]], dtype=precision)
+        
         Y = tf.Variable([[0, -1j], [1j, 0]], dtype=precision)
         Z = tf.Variable([[1, 0], [0, -1]], dtype=precision)
 
-        H = kron(X,X) + kron(Y,Y) + kron(Z,Z)
-        H = tf.cast(self.u, precision)*H
+        H = self.u1*kron(X,X) + self.u2*kron(Y,Y) + self.u3*kron(Z,Z)
+        H = tf.cast(H, precision)
 
         I = tf.eye(self.d, dtype=precision)
         HH = (kron(H, I) - kron(I, tf.transpose(H)))
         HH = tf.repeat(HH[None, :, :], len(t), axis=0)
         L = -1j*HH + 0.5*()
+        return L
+
+
+class SpinSpin(Liouvillian):
+    def __init__(self):
+        self.d = 4
+        self.rank = 1
+        self.I = tf.eye(2, dtype=precision)
+        self.u1 = tf.Variable(tf.random.normal([], 0, 1), trainable=True)
+        self.u2 = tf.Variable(tf.random.normal([], 0, 1), trainable=True)
+        self.u3 = tf.Variable(tf.random.normal([], 0, 1), trainable=True)
+        
+
+        self.degree = 3
+        self.theta1_x = tf.Variable(tf.random.normal([self.degree], 0, 1), trainable=True)
+        self.theta1_y = tf.Variable(tf.random.normal([self.degree], 0, 1), trainable=True)
+        self.theta2_x = tf.Variable(tf.random.normal([self.degree], 0, 1), trainable=True)
+        self.theta2_y = tf.Variable(tf.random.normal([self.degree], 0, 1), trainable=True)
+
+        self.parameter_list = [self.u1, self.u2, self.u3, self.theta1_x, self.theta1_y, self.theta2_x, self.theta2_y]
+
+    def __call__(self, t):
+        u1 = tf.cast(self.u1, precision)
+        u2 = tf.cast(self.u2, precision)
+        u3 = tf.cast(self.u3, precision)
+        theta1_x = tf.cast(self.theta1_x, precision)
+        theta1_y = tf.cast(self.theta1_y, precision)
+        theta2_x = tf.cast(self.theta2_x, precision)
+        theta2_y = tf.cast(self.theta2_y, precision)
+
+        X = tf.Variable([[0, 1], [1, 0]], dtype=precision)
+        Y = tf.Variable([[0, -1j], [1j, 0]], dtype=precision)
+        Z = tf.Variable([[1, 0], [0, -1]], dtype=precision)
+        
+        XX = kron(X, X)
+        YY = kron(Y, Y)
+        ZZ = kron(Z, Z)
+
+        XI = kron(X, self.I)
+        YI = kron(Y, self.I)
+
+        IX = kron(self.I, X)
+        IY = kron(self.I, Y)
+
+        L_list = []
+        for i, t_ in enumerate(t):
+            H = 0.
+            t_ = tf.cast(t_, precision)
+            for j in range(self.degree):
+                H += theta1_x[j]*tf.math.cos((j+1)*t_)*XI + theta1_y[j]*tf.math.cos((j+1)*t_)*YI
+                H += theta2_x[j]*tf.math.cos((j+1)*t_)*IX + theta2_y[j]*tf.math.cos((j+1)*t_)*IY
+            H += u1*kron(X,X) + u2*kron(Y,Y) + u3*kron(Z,Z)
+
+
+            I = tf.eye(self.d, dtype=precision)
+            HH = (kron(H, I) - kron(I, tf.transpose(H)))
+            L = -1j*HH
+            L_list.append(L)
+            
+        L = tf.stack(L_list, axis=0)
         return L
 
 
@@ -69,7 +130,6 @@ class MagnusPropagator(Channel):
             spam = IdealSPAM(self.d)
         self.spam = spam
 
-        self.super_operator = None
         if generate:
             self.generate_channel()
 
@@ -87,14 +147,14 @@ class MagnusPropagator(Channel):
         return state
 
     def super_operator(self, t):
-        t_list = tf.linspace(0., t, self.grid_size)
-        dt = t/self.grid_size
+        dt = t/(self.grid_size-1)
+        t_list = tf.linspace(dt/2, t - dt/2, self.grid_size-1)
 
         L = self.liouvillian(t_list)
         eL = tf.linalg.expm(dt*L)
         T = tf.eye(self.d**2, dtype=precision)
         
-        for i in range(self.grid_size):
+        for i in range(self.grid_size-1):
             T = tf.linalg.matmul(T, eL[i])
 
         return T
